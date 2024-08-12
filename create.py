40b619988fc89393c8164c89db0d81d2081304fb
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2024-08-13 03:26:04 +0700

    update

--- create.py
+++ create.py
@@ -1,3 +1,10 @@
+"""
+    # Copyright © 2022 By Nguyễn Phú Khương
+    # TELEGRAM : @khuongdev0709
+    # Email : dev.phukhuong0709@hotmail.com
+    # Github : npk-0709
+"""
+
 import os
 import subprocess
 import sys
--- resources/core.py
+++ resources/core.py
@@ -1,11 +1,10 @@
 """
     # Copyright © 2022 By Nguyễn Phú Khương
-    # ZALO : 0363561629
+    # TELEGRAM : @khuongdev0709
     # Email : dev.phukhuong0709@hotmail.com
     # Github : npk-0709
 """
 from ui.ui import *
-
 
 
 class Core(Ui_MainWindow):
