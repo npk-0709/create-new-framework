32fc28d681d32b334d78672a5424689383188558
Nguyễn Phú Khương <khuongnguyen07092005@gmail.com>
2025-01-08 03:06:16 +0700

    update gitstart

--- gitstart.ps1
+++ gitstart.ps1
@@ -1,11 +1,25 @@
+param (
+    [string]$mode = "private"
+)
+
 $currentDirectory = Get-Location
 $directoryName = $currentDirectory.Name
+
 echo "Current directory: $currentDirectory"
 echo "Directory name: $directoryName"
 git init
 echo "Git init"
-gh repo create $directoryName --private --source=. --remote=origin
-echo "Create repository"
+
+if ($mode -eq "public") {
+    gh repo create $directoryName --public --source=. --remote=origin
+    echo "Create public repository"
+} elseif ($mode -eq "private") {
+    gh repo create $directoryName --private --source=. --remote=origin
+    echo "Create private repository"
+} else {
+    Write-Host "Invalid mode: $mode. Please use 'public' or 'private'."
+    exit 1
+}
 
 if (Test-Path -Path "Klib\.git") {
     Remove-Item -Path "Klib\.git" -Recurse -Force
@@ -15,7 +29,7 @@ if (Test-Path -Path "Klib\.git") {
 git add .
 echo "Add all files"
 
-git commit -m update
+git commit -m "frist commit"
 echo "Commit"
 
 git branch -m main
